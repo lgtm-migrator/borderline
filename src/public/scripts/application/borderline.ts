3c3e3bb4cc0bd14f847fec44cbf52e1ee028d2ea
Florian Guitton <f.guitton@imperial.ac.uk>
2016-10-15 00:31:14 +0100

    Adding basic page styling and structure

---
+++ src/public/scripts/application/borderline.ts
@@ -0,0 +1,12 @@
+import * as $ from 'jquery';
+
+class Borderline {
+
+    constructor() {
+        $(document).ready(() => {
+            console.log('Hello from app/scripts/ts/Main.ts');
+        });
+    }
+}
+
+var borderline: Borderline = new Borderline();
