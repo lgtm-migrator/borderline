023ba27572a907d2e73a9b9540b17379e3fd4000
Florian Guitton <f.guitton@imperial.ac.uk>
2016-10-18 20:21:07 +0100

    Adding angular typings for the compiler sake

---
+++ src/typings/@angular.d.ts
@@ -0,0 +1,6 @@
+declare module '@angular/common';
+declare module '@angular/core';
+declare module '@angular/forms';
+declare module '@angular/http';
+declare module '@angular/platform-browser';
+declare module '@angular/platform-browser-dynamic';
