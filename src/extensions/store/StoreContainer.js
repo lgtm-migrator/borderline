e10a377bb133e0c8fdcad3c2bf5b30bf919c4d38
Florian Guitton <f.guitton@imperial.ac.uk>
2017-02-17 14:39:53 +0000

    Typo + version bump

--- src/extensions/store/StoreContainer.js
+++ src/extensions/store/StoreContainer.js
@@ -2,18 +2,9 @@ import { Component } from 'react';
 
 class StoreContainer extends Component {
 
-    constructor() {
-        super();
-
-        throw 'Toto';
+    render() {
+        throw 'Extension manager is not implemented';
     }
-
-    componentDidMount() {
-    }
-
-    // render() {
-    //     throw 'Extension manager is not implemented';
-    // }
 }
 
 export default StoreContainer;
