3748cadda80ee3c9de5e80f20dfe273262b66811
Tezirg <jean.grizet16@imperial.ac.uk>
2017-02-23 14:24:43 +0000

    Options method to recursively create folder if they dont exists. Used for pluginFileSystemFolder & pluginSourceFolder

--- src/core/options.js
+++ src/core/options.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs-extra');
 
 var Options = function(configuration) {
     this.mongoUrl = configuration.mongoUrl ? configuration.mongoUrl : 'mongodb://root:root@127.0.0.1:27020/borderline';
@@ -7,6 +8,23 @@ var Options = function(configuration) {
     this.pluginFileSystemFolder = configuration.pluginFileSystemFolder ? configuration.pluginFileSystemFolder : './.plugins/filesystem';
     this.borderlineUiFolder =  configuration.borderlineUiFolder ? configuration.borderlineUiFolder : './node-modules/borderline-ui';
     this.development = configuration.development ? configuration.development : false;
+
+    this._ensureFolder(this.pluginSourcesFolder);
+    this._ensureFolder(this.pluginFileSystemFolder);
+};
+
+
+Options.prototype._ensureFolder = function(inputPath) {
+    var absolutePath = path.resolve(inputPath);
+    var paths = absolutePath.split(path.sep);
+    var currentPath = paths[0];
+    for (var i = 1; i < paths.length; i++) {
+        currentPath = path.resolve(path.join(currentPath, paths[i]));
+        console.log(currentPath);
+        if (fs.existsSync(currentPath) === false) {
+            fs.mkdirSync(currentPath);
+        }
+    }
 };
 
 module.exports = Options;
--- src/core/plugin/filesystem.js
+++ src/core/plugin/filesystem.js
@@ -8,13 +8,8 @@ function PluginFileSystem(pluginUuid) {
 }
 
 PluginFileSystem.prototype._bootstrap = function() {
-    var paths = this.root.split(path.sep);
-    var p = '/';
-    for (var i = 1; i < paths.length; i++) {
-        p = path.join(p, paths[i]);
-        if (fs.existsSync(p) === false) {
-            fs.mkdirSync(p);
-        }
+    if (fs.existsSync(this.root) === false) {
+        fs.mkdirSync(this.root);
     }
 };
 
