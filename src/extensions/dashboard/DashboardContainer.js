71d683100e6028780c086038e16ad7ffe2cc8ddf
Florian Guitton <f.guitton@imperial.ac.uk>
2017-03-10 15:07:03 +0800

    Moving context scene to the onClick component rendering.

--- src/extensions/dashboard/DashboardContainer.js
+++ src/extensions/dashboard/DashboardContainer.js
@@ -3,11 +3,17 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  * ---------------------------------------------------------------------------------------- */
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes as T } from 'react';
 
 class DashboardContainer extends Component {
 
+    // Types for available context
+    static contextTypes = {
+        dispatch: T.func
+    };
+
     componentDidMount() {
+        this.context.dispatch({type: 'DASHBOARD_DID_MOUNT'});
     }
 
     render() {
