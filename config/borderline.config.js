f08a82589943345abb463f7fb150b663e80649f9
Florian Guitton <f.guitton@imperial.ac.uk>
2017-03-17 11:33:32 +0800

    Adding reference configuration file for database access

---
+++ config/borderline.config.js
@@ -0,0 +1,8 @@
+/* -------------------------------------------------------------------------------------------
+ *  Copyright (c) Florian Guitton. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ * ---------------------------------------------------------------------------------------- */
+
+module.exports = {
+    mongoURL: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]'
+};
