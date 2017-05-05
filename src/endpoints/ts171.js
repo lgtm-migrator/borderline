da2b236ebfe47cfffd9f86cece93c1c16cc59e09
Tezirg <jean.grizet16@imperial.ac.uk>
2017-05-05 14:46:56 +0100

    Execution support for TS17.1, with token renew when needed

--- src/endpoints/ts171.js
+++ src/endpoints/ts171.js
@@ -5,26 +5,45 @@ function Transmart171(queryCollection) {
     this.queryCollection = queryCollection;
 
     //Bind member functions
-    this.getToken = Transmart171.prototype.getToken.bind(this);
+    this.ensureAuthentication = Transmart171.prototype.ensureAuthentication.bind(this);
+    this.needsAuthentication = Transmart171.prototype.needsAuthentication.bind(this);
+    this.updateToken = Transmart171.prototype.updateToken.bind(this);
     this.saveQuery = Transmart171.prototype.saveQuery.bind(this);
     this.execute = Transmart171.prototype.execute.bind(this);
 
 }
 
+Transmart171.prototype.needsAuthentication = function(queryModel) {
+    //Needs re-auth if Oauth token details are missing
+  if (queryModel.credentials.hasOwnProperty('access_token') === false ||
+      queryModel.credentials.hasOwnProperty('expires_in') === false ||
+      queryModel.credentials.hasOwnProperty('generated') === false)
+      return true;
+
+  var now = new Date();
+  //compute expiration date for this token
+  var expires = new Date(queryModel.credentials.generated);
+  expires.setTime(expires.getTime() +  queryModel.credentials['expires_in'] * 1000);
+
+  //Compares now and expiration date
+  return (now >= expires);
+
+};
+
 Transmart171.prototype.saveQuery = function(queryModel) {
     var _this = this;
     return new Promise(function(resolve, reject) {
         _this.queryCollection.findOneAndReplace({_id: new ObjectID(queryModel['_id'])}, queryModel).then(function() {
-            resolve(queryModel);
+            resolve(queryModel); //Good, pass the model through
         }, function(error) {
-            reject(error);
+            reject(error); //DB findAndReplace fails
         })
     });
 };
 
-Transmart171.prototype.getToken = function(queryModel) {
+Transmart171.prototype.updateToken = function(queryModel) {
     return new Promise(function(resolve, reject) {
-        //Get new credentials
+        //Get new credentials from data source
         request.post({
             method: 'POST',
             json: true,
@@ -33,15 +52,53 @@ Transmart171.prototype.getToken = function(queryModel) {
             '&username=' + queryModel.credentials.username +
             '&password=' + queryModel.credentials.password
         }, function (error, response, body) {
-            if (!response) {
-                reject(error);
-                return;
+            if (!response) { //Reject on errors
+                reject(error); return;
             }
-            console.log('TS17.1 authentication: ');
-            console.log(body);
-            var newCredentials = Object.assign({}, queryModel.credentials, body);
+            //Update queryModel
+            var newCredentials = Object.assign({}, queryModel.credentials, body, {generated: new Date()});
             var authQuery = Object.assign({}, queryModel, {credentials: newCredentials});
             resolve(authQuery);
+        });
+    });
+};
+
+Transmart171.prototype.ensureAuthentication = function(queryModel) {
+    var _this = this;
+    return new Promise(function(resolve, reject) {
+        if (_this.needsAuthentication(queryModel) == true) {
+            _this.updateToken(queryModel)
+                .then(_this.saveQuery,
+                    function(error) {
+                        reject(error);
+                })
+                .then(function(result) {
+                    resolve(result);
+                }, function(error) {
+                    reject(error);
+                });
+        }
+        else {
+            resolve(queryModel);
+        }
+    });
+};
+
+Transmart171.prototype.perform = function(queryModel) {
+    return new Promise(function(resolve, reject) {
+        request.get({
+            json: true,
+            baseUrl: queryModel.endpoint.sourceHost + ':' + queryModel.endpoint.sourcePort,
+            uri: queryModel.query['uri'] + JSON.stringify(queryModel.query['params']),
+            headers: {
+               Authorization: 'Bearer ' + queryModel.credentials['access_token']
+            }
+        },  function(error, response, body) {
+            if (!response) {
+                reject(error); return;
+            }
+            //Success, resolve with the result data
+            resolve(body);
         });
     });
 };
@@ -50,11 +107,15 @@ Transmart171.prototype.execute = function(queryModel) {
     var _this = this;
 
     return new Promise(function(resolve, reject) {
-        _this.getToken(queryModel).then(function(queryModel) {
-            resolve(queryModel);
-        }, function(error) {
-            reject(error);
-        })
+        _this.ensureAuthentication(queryModel)
+            .then(_this.perform, function(error) {
+                reject(error);
+            })
+            .then(function(result) {
+                resolve(result); //All good !
+            }, function(error) {
+               reject(error);
+            });
     });
 };
 
