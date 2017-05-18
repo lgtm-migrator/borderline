244837845054ba4f9a11e7db7c35308c4326e784
Tezirg <jean.grizet16@imperial.ac.uk>
2017-05-18 10:01:12 +0100

    Adds a port field to 8080 into the sample config file

--- config/borderline.sample.config.js
+++ config/borderline.sample.config.js
@@ -7,5 +7,6 @@ module.exports = {
     mongoURL: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]',
     objectStorageUrl: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]',
     extensionSourcesFolder: '.extensions/sources',
-    extensionFileSystemFolder: '.extensions/filesystem'
+    extensionFileSystemFolder: '.extensions/filesystem',
+    port: 8080
 };
