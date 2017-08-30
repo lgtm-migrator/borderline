6d6bc76ad9a438b64a8f1d7ce58eb785ff9371df
Florian Guitton <f.guitton@imperial.ac.uk>
2017-08-30 14:50:21 +0100

    Adding babel decorator support and requestAnimationFrame polyfill for jest testing

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -5,5 +5,6 @@ const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
     presets: [require.resolve('babel-preset-react-app')],
+    plugins: [require.resolve('babel-plugin-transform-decorators-legacy')],
     babelrc: false,
 });
