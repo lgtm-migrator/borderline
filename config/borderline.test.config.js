726a51b98b95bf9996f96a72e34eb70e1620b92e
Tezirg <jean.grizet16@imperial.ac.uk>
2017-08-30 11:55:40 +0100

    Update patch version and fix typo in prject package.json. Adds travis config with docker config for test cases. (Just running eslint for now)

---
+++ config/borderline.test.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURL: 'mongodb://mongodb/borderline',
+    objectStorageURL: 'mongodb://mongodb/borderlineobject',
+    extensionSourcesFolder: '.plugins/sources',
+    extensionFileSystemFolder: '.plugins/filesystem',
+    development: true,
+    port: 3000
+};
