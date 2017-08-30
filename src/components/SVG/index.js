a136559bed6c629b9da0b30de76f2d2612294cda
Florian Guitton <f.guitton@imperial.ac.uk>
2017-08-30 10:03:43 +0100

    Removing rogue copyright headers

--- src/components/SVG/index.js
+++ src/components/SVG/index.js
@@ -1,8 +1,3 @@
-/* -------------------------------------------------------------------------------------------
- *  Copyright (c) Florian Guitton. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- * ---------------------------------------------------------------------------------------- */
-
 import SVGHandle from './SVG';
 
 export default SVGHandle;
