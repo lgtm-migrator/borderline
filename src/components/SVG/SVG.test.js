28895f47509d99c74ac266d200838ba898cc5e8d
Florian Guitton <f.guitton@imperial.ac.uk>
2017-09-13 17:43:25 +0100

    Consolidating style between Pager's navigation buttons

--- src/components/SVG/SVG.test.js
+++ src/components/SVG/SVG.test.js
@@ -5,7 +5,7 @@ import SVG from './SVG';
 describe('must sanitize the DOM', () => {
 
     let component = null
-    const response = fetch.mockResponseSuccessOnce('<script></script><svg></svg>');
+    const response = fetch.mockResponseSuccessOnce('<script></script><svg xmlns="http://www.w3.org/2000/svg"></svg>');
 
     it('returns <div> before fetch complete', async () => {
         component = renderer.create(<SVG src="mock" />);
