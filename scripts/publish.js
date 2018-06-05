1bce98d3962d1ef14213ba463c0f6a1a804824c7
Florian Guitton <f.guitton@imperial.ac.uk>
2018-06-05 15:36:58 +0100

    Display unhandled error stacktraces

--- scripts/publish.js
+++ scripts/publish.js
@@ -7,6 +7,7 @@ process.env.NODE_ENV = 'production';
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
+    console.error(err.stack);
     throw err;
 });
 
