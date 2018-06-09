b4f7d30c9768b57522f6e09d78aac6d23720a38d
Florian Guitton <f.guitton@imperial.ac.uk>
2018-06-09 18:33:23 +0100

    Doing some linting

--- bin/borderline-devutils.js
+++ bin/borderline-devutils.js
@@ -4,8 +4,8 @@
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
-  console.error(err.stack);
-  throw err;
+    console.error(err.stack);
+    throw err;
 });
 
 const spawn = require('react-dev-utils/crossSpawn');
@@ -18,37 +18,37 @@ const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 
 switch (script) {
-  case 'build':
-  case 'publish':
-  case 'start':
-  case 'test': {
-    const result = spawn.sync(
-      'node',
-      nodeArgs
-        .concat(require.resolve('../scripts/' + script))
-        .concat(args.slice(scriptIndex + 1)),
-      { stdio: 'inherit' }
-    );
-    if (result.signal) {
-      if (result.signal === 'SIGKILL') {
-        console.log(
-          'The build failed because the process exited too early. ' +
-            'This probably means the system ran out of memory or someone called ' +
-            '`kill -9` on the process.'
+    case 'build':
+    case 'publish':
+    case 'start':
+    case 'test': {
+        const result = spawn.sync(
+            'node',
+            nodeArgs
+                .concat(require.resolve('../scripts/' + script))
+                .concat(args.slice(scriptIndex + 1)),
+            { stdio: 'inherit' }
         );
-      } else if (result.signal === 'SIGTERM') {
-        console.log(
-          'The build failed because the process exited too early. ' +
-            'Someone might have called `kill` or `killall`, or the system could ' +
-            'be shutting down.'
-        );
-      }
-      process.exit(1);
+        if (result.signal) {
+            if (result.signal === 'SIGKILL') {
+                console.log(
+                    'The build failed because the process exited too early. ' +
+                    'This probably means the system ran out of memory or someone called ' +
+                    '`kill -9` on the process.'
+                );
+            } else if (result.signal === 'SIGTERM') {
+                console.log(
+                    'The build failed because the process exited too early. ' +
+                    'Someone might have called `kill` or `killall`, or the system could ' +
+                    'be shutting down.'
+                );
+            }
+            process.exit(1);
+        }
+        process.exit(result.status);
+        break;
     }
-    process.exit(result.status);
-    break;
-  }
-  default:
+    default:
         console.log(`Unknown command "${script}".`);
-    break;
+        break;
 }
