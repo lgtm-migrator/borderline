fea85eec6c511235c9fb2f86bdec8c6ed6693f8c
Florian Guitton <f.guitton@imperial.ac.uk>
2018-06-27 16:57:05 +0100

    Moving `borderline-ui` to its package folder

---
+++ packages/borderline-ui/src/extensions/index.js
@@ -0,0 +1,11 @@
+export default [
+    import('./dashboard'),
+    import('./workflow'),
+    import('./account'),
+    import('./magnifier'),
+    import('./theme'),
+    import('./eae'),
+    import('./file'),
+    import('./transmart'),
+    import('./info')
+];
