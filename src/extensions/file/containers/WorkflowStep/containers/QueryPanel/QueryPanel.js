ad86c63482598f9c41da690f2a71631305d5569b
Florian Guitton <f.guitton@imperial.ac.uk>
2018-06-29 14:36:56 +0100

    Allowing file preview from file extension

---
+++ src/extensions/file/containers/WorkflowStep/containers/QueryPanel/QueryPanel.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { default as T } from 'prop-types';
+import { stateAware } from 'utilities/storeManager';
+import Editor from 'components/Editor';
+import { actions } from '../../../../flux';
+
+@stateAware(state => ({
+    fileText: state.stepObject !== undefined && state.stepObject.context !== undefined ? state.stepObject.context.fileText : ''
+}))
+class QueryPanel extends Component {
+
+    // Custom name for container
+    static displayName = 'QueryPanel';
+
+    // Types for available context
+    static contextTypes = {
+        dispatch: T.func
+    };
+
+    componentDidMount() {
+        this.prevEditorValue = this.editorValue;
+        this.autoSave = setInterval(this.saveFileText, 5000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.autoSave);
+        this.saveFileText();
+        this.context.dispatch(actions.clearCurrentStep());
+    }
+
+    saveFileText = () => {
+        if (this.editorValue !== this.prevEditorValue) {
+            this.context.dispatch(actions.saveFileText(this.editorValue));
+            this.prevEditorValue = this.editorValue;
+        }
+    }
+
+    valueChange = (value) => {
+        this.editorValue = value;
+    }
+
+    render() {
+        const { fileText } = this.props;
+        return <Editor language="vs" onChange={this.valueChange} value={fileText} />;
+    }
+}
+
+export default QueryPanel;
--- src/extensions/file/flux.js
+++ src/extensions/file/flux.js
@@ -1,7 +1,29 @@
 import { of, concat } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { mergeMap, mapTo } from 'rxjs/operators';
 import { UploadStage } from './containers/WorkflowStep';
 import { TextStage } from './containers/WorkflowStep';
+
+const types = {
+
+    FILE_STEP_HYDRATE: 'FILE_STEP_HYDRATE',
+    FILE_STEP_HYDRATE_SUCCESS: 'FILE_STEP_HYDRATE_SUCCESS',
+    FILE_STEP_CLEAR: 'FILE_STEP_CLEAR',
+    FILE_STEP_EXECUTE_SUCCESS: 'FILE_STEP_EXECUTE_SUCCESS',
+    FILE_STEP_EXECUTE_FAILURE: 'FILE_STEP_EXECUTE_FAILURE',
+    FILE_FETCH_RESULT_SUCCESS: 'FILE_FETCH_RESULT_SUCCESS',
+    FILE_FETCH_RESULT_FAILURE: 'FILE_FETCH_RESULT_FAILURE',
+    FILE_QUERY_PANEL_UPDATE: 'FILE_QUERY_PANEL_UPDATE',
+    FILE_QUERY_EXECUTE: 'FILE_QUERY_EXECUTE',
+    FILE_QUERY_EXECUTE_SUCCESS: 'FILE_QUERY_EXECUTE_SUCCESS',
+    FILE_QUERY_EXECUTE_FAILURE: 'FILE_QUERY_EXECUTE_FAILURE',
+    FILE_QUERY_POLL: 'FILE_QUERY_POLL',
+    FILE_QUERY_LOAD: 'FILE_QUERY_LOAD',
+    FILE_QUERY_LOAD_SUCCESS: 'FILE_QUERY_LOAD_SUCCESS',
+    FILE_QUERY_LOAD_FAILURE: 'FILE_QUERY_LOAD_FAILURE',
+    FILE_QUERY_FINISHED_SUCCESS: 'FILE_QUERY_FINISHED_SUCCESS',
+    FILE_QUERY_FINISHED_FAILURE: 'FILE_QUERY_FINISHED_FAILURE',
+    FILE_QUERIES_DID_LOAD: 'FILE_QUERIES_DID_LOAD'
+};
 
 export const actions = {
 
@@ -25,6 +47,47 @@ export const actions = {
             outputs: ['text_result'],
             stage: TextStage
         }
+    }),
+
+    getCurrentStep: () => ({
+        type: '@@extensions/workflow/STEP_FETCH',
+        replyTo: types.FILE_STEP_HYDRATE
+    }),
+
+    clearCurrentStep: () => ({
+        type: types.FILE_STEP_CLEAR
+    }),
+
+    updateStepStatus: (step, name, data) => ({
+        type: '@@extensions/workflow/STEP_STATUS_UPDATE',
+        status: {
+            step: step,
+            name: name,
+            data: data
+        }
+    }),
+
+    saveFileText: (fileText) => ({
+        type: types.FILE_QUERY_PANEL_UPDATE,
+        fileText: fileText
+    }),
+
+    saveStep: (step) => ({
+        type: '@@extensions/workflow/STEP_UPDATE',
+        step: step
+    }),
+
+    receiveStepSuccess: () => ({
+        type: types.FILE_STEP_HYDRATE_SUCCESS
+    }),
+
+    fetchResultSuccess: (data) => ({
+        type: types.FILE_FETCH_RESULT_SUCCESS,
+        data: data
+    }),
+
+    fetchResultFailure: () => ({
+        type: types.FILE_FETCH_RESULT_SUCCESS
     })
 };
 
@@ -47,13 +110,68 @@ export const epics = {
                     of(actions.dockTextToWorkflow())
                 )
             )),
+
+    receiveStep:
+        (action) => action.ofType(types.FILE_STEP_HYDRATE)
+            .pipe(mapTo(actions.receiveStepSuccess())),
+
+    receiveStepSuccess:
+        (action, state) => action.ofType(types.FILE_STEP_HYDRATE_SUCCESS)
+            .pipe(mergeMap(() => of(actions.updateStepStatus(state.stepObject._id, 'finished')))),
+
+    queryPanelUpdate:
+        (action, state) => action.ofType(types.FILE_QUERY_PANEL_UPDATE)
+            .pipe(mergeMap(() => of(actions.saveStep(state.stepObject)))),
+
+    fetchResult:
+        (action, state) => action.ofType('FETCH_STEP_RESULT')
+            .pipe(mergeMap((action) => of({ type: `@@extensions/${action.__origin__}/STEP_RESULT`, result: state.previousStepObject.context.fileText })))
+};
+
+const initial = {
+    previousStepObject: null,
+    stepObject: null,
+    queryList: {}
 };
 
 export const reducers = {
+
+    fileReducer:
+        (state = initial, action) => {
+
+            switch (action.type) {
+                case types.FILE_STEP_HYDRATE:
+                    return hydrateFile(state, action);
+                case types.FILE_QUERY_PANEL_UPDATE:
+                    return queryPanelUpdate(state, action);
+                case types.FILE_STEP_CLEAR:
+                    return clearCurrentStep(state);
+                case 'STOP':
+                    return initial;
+                default:
+                    return state;
+            }
+        }
 };
 
-export default {
-    actions,
-    epics,
-    reducers
+const hydrateFile = (state, action) => {
+    state.stepObject = action.step;
+    if (state.stepObject.context.fileText === undefined)
+        state.stepObject.context.fileText = state.stepObject.context.input || '';
+    return state;
+};
+
+const clearCurrentStep = (state) => {
+    state.previousStepObject = state.stepObject;
+    state.stepObject = {};
+    state.queryList = {};
+    return state;
+};
+
+const queryPanelUpdate = (state, action) => {
+    if (state.stepObject.context === undefined)
+        state.stepObject.context = {};
+    state.stepObject.context.fileText = action.fileText;
+    state.stepObject.context.changeFromOriginal = true;
+    return state;
 };
